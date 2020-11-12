9da49e83edf334abc1b74eea0846dd8e9c6946c2
passpos <passpos@outlook.com>
2020-11-12 22:00:01 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-graphics-scene/src/main/java/jfx/scene/layout/box/HBoxApp.java
+++ jfx-graphics-scene/src/main/java/jfx/scene/layout/box/HBoxApp.java
@@ -18,7 +18,14 @@
 package jfx.scene.layout.box;
 
 /**
- *
+ * 水平排列子组件的布局方式
+ * 1. 当未设置子组件时，HBox的尺寸为0，而不是填充父组件；
+ * 2. 若设置了子组件，HBox的尺寸就会被撑开；
+ * 3. 当HBox的水平尺寸较小且不可调整（设置了maxSize），而子组件的总尺寸较大时 ：
+ * - 若每个子组件的尺寸可调整（只设置了prefSize），垂直方向会压缩子组件，水平方
+ * 向也被压缩；
+ * - 若每个子组件的尺寸不可调整（设置了minSize），则子组件不会被挤压，HBox的水
+ * 平尺寸会被强制撑开，但不会溢出窗体；
  * @author realpai <paiap@outlook.com>
  */
 public class HBoxApp {
