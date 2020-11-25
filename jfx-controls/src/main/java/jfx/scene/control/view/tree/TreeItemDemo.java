4eb65ddb8afaf6afff6f6dcf3e5694ccd5822312
passpos <passpos@outlook.com>
2020-11-26 07:34:20 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeItemDemo.java
+++ jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeItemDemo.java
@@ -5,6 +5,9 @@
  */
 package jfx.scene.control.view.tree;
 
+import java.io.File;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import jfx.core.app.ContentBox;
@@ -35,5 +38,83 @@ public class TreeItemDemo extends ContentBox {
         tv.setRoot(root);
         tv.setPrefHeight(50);
         getChildren().add(tv);
+    }
+
+    private TreeView<File> buildFileSystemBrowser() {
+        TreeItem<File> fr = createNode(new File("/"));
+        return new TreeView<>(fr);
+    }
+
+    /**
+     * 此方法创建一个TreeItem来表示给定的File。它通过匿名重写
+     * TreeItem.getChildren（）和TreeItem.isLeaf（）方法来做到这一点，但最好通过
+     * 创建TreeItem的“ FileTreeItem”子类来抽象该方法。
+     * 但是，这留给用户作练习；
+     */
+    private TreeItem<File> createNode(final File f) {
+        TreeItem<File> ti = new TreeItem<>(f) {
+            /**
+             * 我们缓存文件是否为叶。 如果文件不是目录并且其中不包含任何文件，则
+             * 它是叶。
+             * 我们经常调用isLeaf（）来缓存它，因此对File进行实际检查非常昂贵。
+             */
+            private boolean isLeaf;
+
+            /**
+             * 我们对子项和叶子仅进行一次测试，然后将其设置为false，以便在此运行
+             * 期间不再进行检查。
+             * 可能需要更完整的实现来处理更多动态文件系统情况（例如，在显示
+             * TreeView之后，文件夹中添加了文件）。再次，这留给读者作为练习。
+             */
+            private boolean isFirstTimeChildren = true;
+            private boolean isFirstTimeLeaf = true;
+
+            @Override
+            public ObservableList<TreeItem<File>> getChildren() {
+                if (isFirstTimeChildren) {
+                    isFirstTimeChildren = false;
+
+                    // First getChildren() call, so we actually go off and
+                    // determine the children of the File contained in this TreeItem.
+                    super.getChildren().setAll(buildChildren(this));
+                }
+                return super.getChildren();
+            }
+
+            /**
+             * 若是文件，则为叶节点；
+             *
+             * @return
+             */
+            @Override
+            public boolean isLeaf() {
+                if (isFirstTimeLeaf) {
+                    isFirstTimeLeaf = false;
+                    File f = (File) getValue();
+                    isLeaf = f.isFile();
+                }
+
+                return isLeaf;
+            }
+
+            private ObservableList<TreeItem<File>> buildChildren(TreeItem<File> TreeItem) {
+                File f = TreeItem.getValue();
+                if (f != null && f.isDirectory()) {
+                    File[] files = f.listFiles();
+                    if (files != null) {
+                        ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
+
+                        for (File childFile : files) {
+                            children.add(createNode(childFile));
+                        }
+
+                        return children;
+                    }
+                }
+
+                return FXCollections.emptyObservableList();
+            }
+        };
+        return ti;
     }
 }
