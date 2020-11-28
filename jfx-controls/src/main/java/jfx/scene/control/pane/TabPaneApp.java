da3b54bd43c1e5d650e52d97096f1abe7372e3ae
passpos <passpos@outlook.com>
2020-11-28 08:35:24 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/src/main/java/jfx/scene/control/pane/TabPaneApp.java
+++ jfx-controls/src/main/java/jfx/scene/control/pane/TabPaneApp.java
@@ -18,18 +18,16 @@ package jfx.scene.control.pane;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Side;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import jfx.core.app.ContentBox;
 
 /**
- *
+ * TabPane是Control类型
+ * Tab则是直接继承的Object，必须装入TabPane中；
  * @author realpai <paiap@outlook.com>
  */
 public class TabPaneApp extends ContentBox {
@@ -40,23 +38,21 @@ public class TabPaneApp extends ContentBox {
 
     @Override
     public void index() {
-        tp = new TabPane();     // 节点类型
+        tp = new TabPane();
         baseDemo();
-
-        // 标签样式
-        tabStyleDemo();
-
-        // 标签事件
-        eventDemo();
     }
 
     public void baseDemo() {
-        Tab tab = new Tab("tab1");     // 直接继承Object
+        // Tab Content
         HBox hb = new HBox();
         hb.getChildren().addAll(new Button("btn1"), new Button("btn2"));
+
+        // Tab
+        Tab tab = new Tab("tab1");
         tab.setContent(hb);
 
         tp.getTabs().add(tab);
+        getChildren().add(tp);
 
         tp.getSelectionModel().select(tab);    // 默认选中；
         tp.setSide(Side.TOP);                   // 设置标签位置（默认是在顶部）；
@@ -76,49 +72,6 @@ public class TabPaneApp extends ContentBox {
 
         });
 
-        this.getChildren().add(tp);
     }
 
-    public void tabStyleDemo() {
-        Tab tab = new Tab();
-        tab.setText("tab2");
-        ImageView iv = new ImageView("file:D:\\Projects\\JavaFX\\fx-control\\src\\main\\java\\icon\\fav.jpg");
-        iv.setFitWidth(15);
-        iv.setFitHeight(15);
-        tab.setGraphic(iv);
-        // 标签不可关闭，关闭按钮也会消失；
-        tab.setClosable(false);
-        // 所有标签不可关闭；
-        // tp.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-
-        tp.getTabs().add(tab);
-    }
-
-    public void eventDemo() {
-
-        Tab tab = new Tab("tab3");
-        // 禁用标签；
-//        tab3.setDisable(true);
-        // 选中改变事件。由于涉及到两种状态（选中/未选中），所以（切入/切离tab3）都会运行下列事件处理；
-        tab.setOnSelectionChanged(new EventHandler<Event>() {
-            @Override
-            public void handle(Event t) {
-                Tab tb = (Tab) t.getSource();
-                ol("选中了" + tb.getText());
-            }
-        });
-        tab.setOnClosed(new EventHandler<Event>() {
-            @Override
-            public void handle(Event t) {
-                ol("啊！你真的把我关闭了！");
-            }
-        });
-        tab.setOnCloseRequest(new EventHandler<Event>() {
-            @Override
-            public void handle(Event t) {
-                ol("你竟然要关闭我？！哼！");
-            }
-        });
-        tp.getTabs().addAll(tab);
-    }
 }
