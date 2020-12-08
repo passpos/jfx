2cd5d8d68fe0f8338532082c864bb8e273429e61
passpos <passpos@outlook.com>
2020-12-08 11:56:09 +0800

    TreeCell Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeCellApp.java
+++ jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeCellApp.java
@@ -6,7 +6,6 @@
 package jfx.scene.control.view.tree;
 
 import javafx.scene.control.TreeCell;
-import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.util.Callback;
@@ -22,7 +21,6 @@ public class TreeCellApp extends ContentBox {
     public static final boolean SHOWING = false;
     public static final String TITLE = "Tree - TreeCell";
     private TreeView<String> tv;
-    private TreeItem<String> root;
 
     @Override
     public void index() {
@@ -30,26 +28,9 @@ public class TreeCellApp extends ContentBox {
         textFieldTreeCellDemo();
     }
 
-    @SuppressWarnings("unchecked")
     public void fillData() {
-        tv = new TreeView<>();
-
-        root = new TreeItem<>("中国");
-        root.setExpanded(true);
-        root.setValue("China");
-
-        TreeItem<String> ti1 = new TreeItem<>("黑龙江省");
-        TreeItem<String> leaf1 = new TreeItem<>("哈尔滨市");
-        TreeItem<String> leaf2 = new TreeItem<>("佳木斯市");
-        TreeItem<String> leaf3 = new TreeItem<>("大庆市");
-        ti1.getChildren().addAll(leaf1, leaf2, leaf3);
-
-        TreeItem<String> ti2 = new TreeItem<>("福建省");
-        TreeItem<String> ti3 = new TreeItem<>("北京市");
-        TreeItem<String> ti4 = new TreeItem<>("广东省");
-
-        tv.setRoot(root);
-        root.getChildren().addAll(ti1, ti2, ti3, ti4);
+        TreeUtils tu = new TreeUtils();
+        tv = tu.getTreeView();
 
         getChildren().add(tv);
     }
