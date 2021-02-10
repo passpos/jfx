ea14cd17580d44021b598a4ba521a36f9d05dafd
passpos <passpos@outlook.com>
2021-02-11 02:36:03 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeCellApp.java
+++ jfx-controls/src/main/java/jfx/scene/control/view/tree/TreeCellApp.java
@@ -16,13 +16,17 @@ import jfx.core.utils.TreeUtils;
 /**
  * TreeCell - 节点类型，用于填充并传染一个树单元格视图；
  *
- * TreeCell 本身虽然包含了用于展示的数据对象和一个TreeItem对象，但平常情况下，直
- * 接访问它们，将会是null。仅当TreeView渲染完毕后，用户直接操作TreeCell，在触发
- * 的事件动作中，可以访问到它们。
+ * TreeCell 本身虽然包含了用于展示的数据对象、所在TreeView和一个TreeItem对象，平
+ * 常情况下，直接访问它们，将会是null。仅当TreeView渲染完毕后，用户直接操作
+ * TreeCell，在触发的事件动作中，可以访问到它们。
  *
- * 例如，在 tv.setCellFactory(callback) 中，callback.call() 方法要求返回一个
- * TreeCell，这个TreeCell一般由用户进行实例化，之后，通过该 TreeCell 是访问不到
+ * 例如：
+ * 在 tv.setCellFactory(callback) 中，callback.call() 方法要求返回一个TreeCell，
+ * 这个TreeCell一般由用户进行实例化，之后，通过该 TreeCell 是访问不到 TreeView、
  * TreeItem 和 数据 的，但在类似 TreeCell.setOnDragDetected()方法内，就可以了；
+ *
+ * 除此之外，在 TreeCell.updateItem(item, empty)中，则可以通过 item 参数，直接访
+ * 问数据对象；
  *
  *
  * @author passpos <paiap@outlook.com>
