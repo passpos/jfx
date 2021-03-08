64383f3a2ca4fcddf70a47d576369033bba7ba4e
passpos <passpos@outlook.com>
2021-03-09 05:36:08 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

---
+++ jfx-graphics/jfx-scene-layout/src/main/java/jfx/scene/layout/RegionApp.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jfx.scene.layout;
+
+/**
+ * Region extends Parent
+ *
+ * Region的布局范围不受任何 Border 或 Background 的影响；
+ *
+ * 每个区域由几层组成，按从下到上的顺序绘制：
+ * background fills
+ * background images
+ * border strokes
+ * border images
+ * contents
+ *
+ * 直接可用样式
+ * -fx-region-background
+ * -fx-region-border
+ *
+ * -fx-background-***
+ * -fx-border-***
+ *
+ * -fx-padding
+ * -fx-position-shape
+ * -fx-scale-shape
+ * -fx-shape
+ * -fx-snap-to-pixel
+ *
+ * -fx-pref-height
+ * -fx-max-heigh
+ * -fx-min-height
+ *
+ * -fx-pref-width
+ * -fx-max-width
+ * -fx-min-width
+ *
+ *
+ * @author passpos <paiap@outlook.com>
+ */
+public class RegionApp {
+
+}
