34810be1cc7eff286f853d45de77043a51d6b188
passpos <passpos@outlook.com>
2021-03-19 08:11:43 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/jfx-scene-control-view/src/main/java/jfx/scene/control/cell/tree/TreeCellDemo1.java
+++ jfx-controls/jfx-scene-control-view/src/main/java/jfx/scene/control/cell/tree/TreeCellDemo1.java
@@ -6,7 +6,6 @@
 package jfx.scene.control.cell.tree;
 
 import javafx.scene.control.TreeCell;
-import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.util.Callback;
@@ -24,25 +23,23 @@ public class TreeCellDemo1 extends ContentBox {
     public static final boolean SHOWING = false;
     public static final String TITLE = "Tree - TreeCell 节点文本编辑";
     private TreeView<String> tv;
-    private TreeItem<String> root;
 
     @Override
     public void index() {
         TreeUtils tu = new TreeUtils();
         tv = tu.getTreeView();
-        root = tu.getTreeItem();
-
         getChildren().add(tv);
 
-        textFieldTreeCellDemo1();
+        forTreeViewDemo1();
     }
 
     /**
      * TextFieldTreeCell.forTreeView()
      *
      * 静态方法forTreeView()，既支持无参数，也支持用户定义的转换器参数；
+     * 其底层实现类似 textFieldTreeCellDemo()；
      */
-    public void textFieldTreeCellDemo1() {
+    public void forTreeViewDemo1() {
         tv.setEditable(true);
         tv.setCellFactory(TextFieldTreeCell.forTreeView());
     }
@@ -52,7 +49,7 @@ public class TreeCellDemo1 extends ContentBox {
      *
      * StringConverter定义数据对象与节点文本之间的转换策略；
      */
-    public void textFieldTreeCellDemo2() {
+    public void forTreeViewDemo2() {
         StringConverter<String> sc = new StringConverter<>() {
             @Override
             public String toString(String t) {
@@ -73,6 +70,37 @@ public class TreeCellDemo1 extends ContentBox {
         tv.setCellFactory(callback);
     }
 
+    public void textFieldTreeCellDemo() {
+        StringConverter<String> sc = new StringConverter<>() {
+            @Override
+            public String toString(String t) {
+                return t + " - 这个好";
+            }
+
+            @Override
+            public String fromString(String string) {
+                return string;
+            }
+
+        };
+
+        Callback<TreeView<String>, TreeCell<String>> callback;
+        callback = new Callback<TreeView<String>, TreeCell<String>>() {
+            @Override
+            public TreeCell<String> call(TreeView<String> param) {
+                // TextFieldTreeCell<String> tfCell1 = new TextFieldTreeCell<>();
+                TextFieldTreeCell<String> tfCell2 = new TextFieldTreeCell<>(sc);
+                return tfCell2;
+            }
+        };
+
+        tv.setEditable(true);
+        tv.setCellFactory(callback);
+    }
+
+    /**
+     *
+     */
     public void treeCellEditDemo() {
         Callback<TreeView<String>, TreeCell<String>> callback;
         callback = new Callback<TreeView<String>, TreeCell<String>>() {
