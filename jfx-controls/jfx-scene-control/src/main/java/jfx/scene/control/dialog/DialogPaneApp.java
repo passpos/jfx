d9226bf861832baffc580e454c2d4d39ba714b9f
passpos <passpos@outlook.com>
2021-03-29 10:31:11 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-controls/jfx-scene-control/src/main/java/jfx/scene/control/dialog/DialogPaneApp.java
+++ jfx-controls/jfx-scene-control/src/main/java/jfx/scene/control/dialog/DialogPaneApp.java
@@ -3,13 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jfx.scene.control.dialog;
+
+import jfx.core.app.ContentBox;
 
 /**
  *
  * @author passpos <paiap@outlook.com>
  */
-public class DialogPaneApp {
+public class DialogPaneApp extends ContentBox {
+
+    public static final boolean SHOWING = false;
+    public static final String TITLE = "Dialog - DialogPane";
+
+    @Override
+    public void index() {
+    }
 
 }
