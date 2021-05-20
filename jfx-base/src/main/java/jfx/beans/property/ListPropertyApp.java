b83b677cc10931e722d446a32c18936c008ca8e0
passpos <passpos@outlook.com>
2021-05-21 05:25:00 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jfx-base/src/main/java/jfx/beans/property/ListPropertyApp.java
+++ jfx-base/src/main/java/jfx/beans/property/ListPropertyApp.java
@@ -16,35 +16,53 @@
  */
 package jfx.beans.property;
 
-import java.util.Comparator;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import jfx.core.app.ContentBox;
 
 /**
+ * SimpleListProperty<E>
+ * abstract class ListProperty<E>
+ *
+ * java.lang.Object
+ * |—javafx.beans.binding.ListExpression<E>
+ * |——javafx.beans.property.ReadOnlyListProperty<E>
+ * |———javafx.beans.property.ListProperty<E>
+ * |————javafx.beans.property.ListPropertyBase<E>
+ * |—————javafx.beans.property.SimpleListProperty<E>
+ *
+ * SimpleListProperty<String> 与 ObservableList<String>的关系是：
+ * SimpleListProperty<String>仅仅只是一个属性实例，本身并不存储任何List集合数据，
+ * 它是通过 ObservableList<String> 来管理集合数据的。
+ *
+ * 新创建的SimpleListProperty<String>实例本身不会包含 ObservableList<String> 对
+ * 象，它由用户创建并设定，如果不设定，会报告 UnsupportedOperationException ；
  *
  * @author realpai <paiap@outlook.com>
  */
 public class ListPropertyApp extends ContentBox {
 
     public static final boolean SHOWING = false;
-    public static final String TITLE = "Property - ListProperty";
+    public static final String TITLE = "Property - ListProperty ChangeListener";
     private ObservableList<String> list;
     private SimpleListProperty<String> slp;
 
     @Override
     public void index() {
+        base();
+        changeListenerDemo();
+        modifyList();
+    }
+
+    public void base() {
         setConsole();
 
+        // 必须将 list 传入ListProperty，否则报错；
         list = FXCollections.observableArrayList();
         slp = new SimpleListProperty<>(list);
-
-        changeListenerDemo();
-        listChangeListenerDemo();
     }
 
     /**
@@ -60,59 +78,21 @@ public class ListPropertyApp extends ContentBox {
                     ObservableList<String> t,
                     ObservableList<String> t1
             ) {
-//                ob("t：");
-//                t.forEach(ListPropertyApp.this::ob);
-//                ob("t1：");
-//                t1.forEach(ListPropertyApp.this::ob);
+                ob("t：");
+                t.forEach(ListPropertyApp.this::ob);
+
+                ob("-----------------------------------------------------");
+
+                ob("t1：");
+                t1.forEach(ListPropertyApp.this::ob);
             }
 
         });
+    }
 
+    public void modifyList() {
         // 添加元素到列表属性的方法
         list.add("A");
         slp.add("B");
-    }
-
-    /**
-     * 因为list集合有索引，所以需要next()方法
-     */
-    public void listChangeListenerDemo() {
-        slp.addListener(new ListChangeListener<String>() {
-            @Override
-            public void onChanged(ListChangeListener.Change<? extends String> c) {
-                ob(c, 200);
-
-                // 获得更改后的元素列表
-                c.getList().forEach((str) -> ob(str));
-
-                c.reset();
-                // 获得更改的元素（注意：必须调用 next()，否则出错）
-                if (c.next()) {
-                    ob("起始位置：" + c.getFrom(), 200);
-                    ob("结束位置：" + c.getTo(), 200);
-                    ob("wasAdded()：" + c.wasAdded(), 200);
-                    // ob("wasAdded()：" + c.getAddedSubList(), 200);
-                    // ob("wasAdded()：" + c.getAddedSize(), 200);
-                    // ob("wasAdded()：" + c.getRemoved(), 200);
-                    ob("wasRemoved()：" + c.wasRemoved(), 200);
-                    ob("wasReplaced()：" + c.wasReplaced(), 200);
-                    // 排序变化
-                    ob("wasPermutated()：" + c.wasPermutated(), 200);
-                    ob("wasUpdated()：" + c.wasUpdated(), 200);
-                }
-            }
-        });
-        slp.add("C");
-        // slp.addAll("C","D");
-        slp.remove("C");
-        slp.set(0, "D");
-        slp.get(0);
-        // 使用 list 而不是 slp（排序过程被拆分），才能激活 wasPermutated()
-        slp.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o2.compareTo(o1);
-            }
-        });
     }
 }
